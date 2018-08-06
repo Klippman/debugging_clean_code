89766e122f41a85e9df027c261e041f271eeed5e
Devon.Klipp <devonklipp@gmail.com>
2018-08-06 11:13:22 -0400

    udated js

--- rock_paper_scis.js
+++ rock_paper_scis.js
@@ -1,72 +1,82 @@
-let gameOver = false;
-let computerChoices = ["rock", "paper", "scissors"];
+// let gameOver = false;
+// let computerChoices = ["rock", "paper", "scissors"];
 
-function randomFrom(array) {
-    return array[(Math.ceil(Math.random()*3)) ];
-}
+// function randomFrom(array) {
+//     return array[(Math.floor(Math.random()*3)) ];
+// }
 
 
-function checkInput(input, computerChoices) {
-    if (input === "quit") {
-        return true;
-  }
+// function checkInput(input, computerChoices) {
+//     if (input === "quit") {
+//         return true;
+//   }
   
-let computerChoice = randomFrom(computerChoices);
+// let computerChoice = randomFrom(computerChoices);
   
-    if(computerChoice === "rock" && input === "scissors"){
-        alert("Computer wins!");
-            return true;
-    } else if (computerChoice === "scissors" && input === "paper"){
-        alert("Computer wins!");
-            return true;
-    } else if (computerChoice === "paper" && input === "rock"){
-        alert("Computer wins!");
-            return true;
-}
+//     if(computerChoice === "rock" && input === "scissors"){
+//         alert("Computer wins!");
+//             return true;
+//     } else if (computerChoice === "scissors" && input === "paper"){
+//         alert("Computer wins!");
+//             return true;
+//     } else if (computerChoice === "paper" && input === "rock"){
+//         alert("Computer wins!");
+//             return true;
+//     }
+    // } else if (computerChoice === "scissors" && input === "rock"){
+    //     alert("User wins!");
+    //         return true;
+    // } else if (computerChoice === "paper" && input === "scissors"){
+    //     alert("User wins!");
+    //         return true;
+    // } else if (computerChoice === "rock" && input === "paper"){
+    //     alert("User wins!");
+    //         return true;
+    // }
 
-        alert("User wins!");
-            return false;
-}
-
-function start(gameOver, computerChoices) {
-    while (!gameOver){
-        let playerInput = '';
-            prompt("Hi! Enter rock/paper/scissors to play, or quit to stop playing.");
-                gameOver = checkInput(playerInput, computerChoices);
-                    playerInput = "paper";
-
-} 
-}
-
+//         alert("User wins!");
+//             return false
 
 
-start(gameOver, computerChoices)
+// }
+
+// function start(gameOver, computerChoices) {
+//     while (!gameOver){
+//         let playerInput = prompt("Hi! Enter rock/paper/scissors to play, or quit to stop playing.");
+//                 gameOver = checkInput(playerInput, computerChoices);
+
+// } 
+// }
+
+
+
+// start(gameOver, computerChoices)
 
 // Problem 2 NYCDA Spy Encoder ------------------------------------------------------------------------------------------------
 
 function encode(word) {
     //somewhere to store the converted characters
-    let result = {};
+    let result = [];
     //next we loop over each character in the string.
-        for(let position of word){
+        for(let i in word){
     //and save the charcode to the array
-            result.push(word.charCodeAt(position-1));
+            result.push(word.charCodeAt(i));
   }
   // and return that list of codes, separated by colons
-            return result.list.join(':');
+            return result.join(':');
   }
   
   function decode(hash) {
     //split the code by semicolons!
-    let arr = hash.splt(':');
+    let arr = hash.split(':');
   // again somewhere to put our codes
-    let result = {};
+    let result = [];
   //loop over the codes
         for(let code of arr){
     //change the code back to the string character equivalent
-            result.push(String.fromCharCode(code+1));
+            result.push(String.fromCharCode(code));
     }
-            return result.list.join('');
+            return result.join('');
   }
   
   
